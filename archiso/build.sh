67ba032fb4f49ca71f767b0bd090ff06ecca610e
Erik Dubois <erik.dubois@gmail.com>
2020-03-17 08:03:43 +0100

    test push

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-next
-iso_label="arcolinux-next-v20.3.4"
+iso_label="arcolinux-next-v20.4.1"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.3.4"
+iso_version="v20.4.1"
 install_dir=arch
 work_dir=work
 out_dir=out
