3942e30d663b2a97058b6d69732946e0c49203dd
Erik Dubois <erik.dubois@gmail.com>
2020-03-27 08:24:06 +0100

    move from next to stable

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-next
-iso_label="arcolinux-next-v20.4.7"
+iso_label="arcolinux-next-v20.4.8"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.4.7"
+iso_version="v20.4.8"
 install_dir=arch
 work_dir=work
 out_dir=out
