05ecc645f0228e45b38679f1d392662905b2c749
Erik Dubois <erik.dubois@gmail.com>
2020-06-24 20:55:43 +0200

    20.7.4 new calamares

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-next
-iso_label="arcolinux-next-v20.7.3"
+iso_label="arcolinux-next-v20.7.4"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.7.3"
+iso_version="v20.7.4"
 install_dir=arch
 work_dir=work
 out_dir=out
