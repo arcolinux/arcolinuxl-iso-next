0a79002be4e80e580d35ebac1f1e24ec6cffa923
Erik Dubois <erik.dubois@gmail.com>
2020-08-20 19:29:50 +0200

    push for Marco

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -103,6 +103,7 @@ sudo cp -r ../../arcolinux-iso-next $buildFolder
 sudo chmod 750 $buildFolder/archiso/airootfs/etc/sudoers.d
 sudo chmod 750 $buildFolder/archiso/airootfs/etc/polkit-1/rules.d
 sudo chgrp polkitd $buildFolder/archiso/airootfs/etc/polkit-1/rules.d
+sudo chmod 750 $buildFolder/archiso/airootfs/root
 
 echo "adding time to /etc/dev-rel"
 date_build=$(date -d now)
@@ -114,12 +115,6 @@ echo "################################################################## "
 echo
 yes | sudo pacman -Scc
 
-echo
-echo "################################################################## "
-tput setaf 2;echo "Phase 5 bis : Linux kernel or linux-lts kernel is chosen on the iso since 11/2019"
-echo "https://wiki.archlinux.org/index.php/Kernel";tput sgr0
-echo "################################################################## "
-echo
 
 echo
 echo "################################################################## "
