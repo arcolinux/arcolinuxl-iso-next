28a95ce6ca625a75619633ba882179131234a9f4
Erik Dubois <erik.dubois@gmail.com>
2020-08-21 10:21:31 +0200

    fix for multiple kernels choice in calamares

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -6,4 +6,6 @@ rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rm /usr/local/bin/arcolinux-all-cores.sh
 mv /etc/arcolinux-release /etc/lsb-release
+pacman -R mkinitcpio-archiso --noconfirm
+mv /etc/mkinitcpio.d/arcolinux /etc/mkinitcpio.d/linux.preset
 rm /usr/local/bin/arcolinux-cleanup.sh
