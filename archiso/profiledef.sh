4c92c3ae70f66f79605b6c4083fc6a87aa74391a
arcolinuxz <arcolinuxinfo@gmail.com>
2021-12-17 12:51:52 +0100

    one # too much in display manager gdm

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -14,7 +14,7 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/gshadow"]="0:0:600"	
+  ["/etc/gshadow"]="0:0:600"
   ["/etc/shadow"]="0:0:600"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
@@ -24,5 +24,5 @@ file_permissions=(
   ["/usr/local/bin/arcolinux-final"]="0:0:755"
   ["/usr/local/bin/arcolinux-remove-nvidia"]="0:0:755"
   ["/usr/local/bin/arcolinux-displaymanager-check"]="0:0:755"
-  ["/usr/local/bin/arcolinux-before"]="0:0:755"
+  ["/usr/local/bin/arcolinux-before"]="0:0:755"  
 )
